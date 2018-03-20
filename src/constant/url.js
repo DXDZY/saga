6d1f1b64a7e195bf651ca127482e1bd572d7b262
dengxiaodong <dengxiaodong@xin.com>
2018-03-20 19:31:54 +0800

    no message

--- src/constant/url.js
+++ src/constant/url.js
@@ -1,4 +1,11 @@
-//获取
-export const GET_POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+let baseUrl='';
+if (process.env.NODE_ENV === 'production') {
+
+}else{
+    baseUrl="/api/"
+}
+// console.log(process.env.NODE_ENV);
+//获取车辆详情信息
+export const GET_POSTS_URL = `${baseUrl}detail.json`;
 //获取用户信息
 export const GET_USER_URL = 'https://jsonplaceholder.typicode.com/users';
