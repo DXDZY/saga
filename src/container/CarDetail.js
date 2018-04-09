cb05b763414f5e119f92f64a13bcde4f0024705e
dengxiaodong <dengxiaodong@xin.com>
2018-04-09 20:32:30 +0800

    no message

--- src/container/CarDetail.js
+++ src/container/CarDetail.js
@@ -7,25 +7,10 @@ import Loading from 'component/Loading';
 import {getCarDetailProps} from 'selector/CarDetail';
 import * as actions from 'action/carDetail';
 
-// const mapStateToProps = (state) => ({
-//     data: state.carDetail.data.data,
-//     isLoading: state.carDetail.isLoading,
-//     payLoad: state.carDetail.payLoad,
-// })
 const mapStateToProps = (state) => getCarDetailProps(state);
 const mapDispatchToProps = (dispatch) => ({
     actions:bindActionCreators(actions,dispatch)
 })
-// const mapDispatchToProps = (dispatch) => ({
-//     getCarDetail: (carid) => dispatch({
-//         type: get_carDetail,
-//         carid,
-//     }),
-//     currentSoundType: (currentSoundType) => dispatch({
-//         type:current_sound_type,
-//         currentSoundType,
-//     }),
-// })
 const LoadableComponent = Loadable({
     loader: () =>
         import ('component/CarDetail'),
