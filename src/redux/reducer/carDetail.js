78633cef73f12ce9f5d68a06051a2780c1308c86
dengxiaodong <dengxiaodong@xin.com>
2018-04-09 20:38:55 +0800

    no message

--- src/redux/reducer/carDetail.js
+++ src/redux/reducer/carDetail.js
@@ -11,52 +11,21 @@ const carDetailReducer = handleActions({
     [actions.getSoundType]: (state, action) => {
         return {
             ...state,
-            currentSoundType:action.payload.currentSoundType,
+            ...action.payload,
         }
     },
+    [actions.getDataError]:(state,action)=>{
+        return {
+            ...state,
+            ...action.payload,
+            isLoading:false,
+        }
+    }
 }, {
     data: {},
     error: null,
     isLoading: true,
     currentSoundType: '-1',
 });
-// import {
-//     get_carDetail_success,
-//     get_carDetail_fail,
-//     current_sound_type,
-// } from 'constant/actionTypes';
 
-// const carDetailReducer = (state = {
-//     data: {},
-//     error: null,
-//     isLoading: true,
-//     payLoad: {
-//         currentSoundType: '-1',
-//     }
-// }, action) => {
-//     switch (action.type) {
-//         case get_carDetail_success:
-//             return {
-//                 ...state,
-//                 data: action.data,
-//                 isLoading: false,
-//             }
-//         case get_carDetail_fail:
-//             return {
-//                 ...state,
-//                 error: action.error,
-//                 isLoading: false,
-//             }
-//         case current_sound_type:
-//             return {
-//                 ...state,
-//                 payLoad:{
-//                     ...state.payLoad,
-//                     currentSoundType: action.currentSoundType,
-//                 }
-//             }
-//         default:
-//             return state;
-//     }
-// }
 export default carDetailReducer;
